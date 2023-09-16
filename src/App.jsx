38d77d79fad0d0da082178b838c7d7a704ae4151
DevAro178 <me.ammad1786@gmail.com>
2023-09-16 22:31:23 +0500

    Applied Lenis Smooth Scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 import "./App.css";
 import BackgroundImg from "./components/BackgroundImg";
 
+import Lenis from "@studio-freight/lenis";
 import Header from "./components/Header";
 import HeroSection from "./components/HeroSection";
 import PasswordGenSection from "./components/PasswordGenSection";
 
 function App() {
+  const lenis = new Lenis();
+
+  // lenis.on("scroll", (e) => {
+  //   console.log(e);
+  // });
+
+  function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+  }
+
+  requestAnimationFrame(raf);
   return (
     <>
       <BackgroundImg />
